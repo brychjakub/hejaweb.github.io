f577839d98d09f3cd4e40570d49dce45eed78bd1
JBrych <jakub.brych@partners.cz>
2025-06-11 16:31:36 +0200

    adding server.py

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+# Vytvoření Flask aplikace
+app = Flask(__name__)
+
+# Nastavení pro PythonAnywhere
+app.config['DEBUG'] = False
+
+@app.route('/')
+def index():
+    """Hlavní stránka"""
+    return send_from_directory('hejaWeb', 'index.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    """Servírování statických souborů"""
+    return send_from_directory('hejaWeb', filename)
+
+@app.route('/assets/<path:filename>')
+def serve_assets(filename):
+    """Servírování asset souborů"""
+    return send_from_directory('hejaWeb/assets', filename)
+
+@app.route('/images/<path:filename>')
+def serve_images(filename):
+    """Servírování obrázků"""
+    return send_from_directory('hejaWeb/images', filename)
+
+# Pro PythonAnywhere je důležité mít tuto podmínku
+if __name__ == '__main__':
+    app.run(debug=True)
